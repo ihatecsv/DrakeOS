59f99faa68b0355d40e197275158c73d9f0e1e30
Drake Luce <me@drakeluce.com>
2019-06-30 19:01:54 -0300

    Print my name endlessly

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,10 +1,23 @@
-loopLabel:
-    mov ah, 0x0e            ; When we interrupt, use "scrolling teletype routine"
+mov ah, 0x0e                ; When we interrupt, use "scrolling teletype routine"
 
-    mov al, 'D'             ; Provide the character for the interrupt
+reset:
+    mov bx, message         ; Set bx to the message address
+    add bx, 0x7c00          ; Offset it by the BIOS boot record offset
+    jmp loopLabel           ; Jump into the loop
+
+loopLabel:
+    mov cx, [bx]            ; Temporarily store the character for cmp
+    cmp cx, 0               ; Check if we've reached the null byte
+    je reset                ; If we have reached the null byte, reset
+    
+    mov al, [bx]            ; Provide the character for the interrupt
     int 0x10                ; Do it
 
-    jmp loopLabel           ; Loop forever dude
+    add bx, 0x1             ; Increment char ptr
+    jmp loopLabel           ; Loop again
+
+message:
+    db "Drake", 0
 
 times 510-($-$$) db 0       ; Pad with zeroes to 512 bytes
 dw 0xaa55                   ; Magic number for boot sector
