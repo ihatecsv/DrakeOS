bbde336f5ec587dae6c0b6f8bd6a587a29bae47d
Drake Luce <me@drakeluce.com>
2019-06-30 17:54:30 -0300

    Print the best alphanumeric character endlessly

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,4 +1,9 @@
 loopLabel:
+    mov ah, 0x0e            ; When we interrupt, use "scrolling teletype routine"
+
+    mov al, 'D'             ; Provide the character for the interrupt
+    int 0x10                ; Do it
+
     jmp loopLabel           ; Loop forever dude
 
 times 510-($-$$) db 0       ; Pad with zeroes to 512 bytes
