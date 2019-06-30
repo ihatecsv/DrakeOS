3218f5f6fc0daa0eb8053ec44e567a89aadef54f
Drake Luce <me@drakeluce.com>
2019-06-30 17:50:01 -0300

    More correct indentation

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,4 +1,5 @@
 loopLabel:
     jmp loopLabel           ; Loop forever dude
-    times 510-($-$$) db 0   ; Pad with zeroes to 512 bytes
-    dw 0xaa55               ; Magic number for boot sector
+
+times 510-($-$$) db 0       ; Pad with zeroes to 512 bytes
+dw 0xaa55                   ; Magic number for boot sector
